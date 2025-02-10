f5cd8735f169b2e462c3d804d23326fb8e128ab9
WarpedEdge <richard.beschinski@gmail.com>
2025-02-10 16:00:24 -0500

    more project files

---
+++ main.py
@@ -0,0 +1,7 @@
+from user import User
+
+app_user_one = User("rb@rb.com", "Richard B", "pwd1", "DevOps Engineer")
+app_user_one.get_user_info()
+
+app_user_two = User("cb@cb.com", "Chloe B", "supersecretmeatballs", "Financial Advisor")
+app_user_two.get_user_info()
